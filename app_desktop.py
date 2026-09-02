af5b12a846f13563ad032b1f5a210cffe4803c01
lobotomyorkms <mcarenascorrea@gmail.com>
2026-09-02 23:06:50 +0200

    Upgraded mp3 quality

--- app_desktop.py
+++ app_desktop.py
@@ -16,7 +16,7 @@ def download_audio(url):
         "postprocessors": [{
             "key": "FFmpegExtractAudio",
             "preferredcodec": "mp3",
-            "preferredquality": "192",
+            "preferredquality": "320",
         }],
         "noplaylist": True,
         "quiet": True,
